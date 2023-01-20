c2e4f5ca662bd5aaa6029dbcb82680c5b18a8c5d
shubham kumar tiwari <shbhmtwr19@gmail.com>
2023-01-20 12:53:10 +0530

    Update main.go

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 
 func main() {
 	ticker := time.Tick(time.Second)
+	
+	fmt.Printf("Collecting pods info")
 	for i := 1; i <= 10; i++ {
 		<-ticker
         fmt.Printf("\rContainer status :  %d/10", i) // escape sequence is different in this environment
